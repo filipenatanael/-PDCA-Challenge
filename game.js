dc772677b6a72636fa8684ea9096c62457050179
Filipe Natanael <filipenatanael@live.com>
2017-06-07 23:56:19 -0300

    ..
    ..

--- game.js
+++ game.js
@@ -7,9 +7,7 @@ var canSpin;
 // slices (prizes) placed in the wheel
 var slices = 4;
 // prize names, starting from 12 o'clock going clockwise
-//var slicePrizes = ["A KEY!!!", "50 STARS", "500 STARS", "BAD LUCK!!!", "200 STARS", "100 STARS", "150 STARS", "BAD LUCK!!!"];
 var slicePrizes = ["P","D","C","A"];
-
 // the prize you are about to win
 var prize;
 // text field where to show the prize
@@ -17,7 +15,6 @@ var prizeText;
 
 window.onload = function() {
 	// creation of a 458x488 game
-	//858, 858
 	game = new Phaser.Game(412, 412, Phaser.AUTO, "gameArea");
 	// adding "PlayGame" state
 	game.state.add("PlayGame",playGame);
@@ -40,8 +37,6 @@ playGame.prototype = {
 		// giving some color to background
 		game.stage.backgroundColor = "#fff";
 		// adding the wheel in the middle of the canvas
-		//wheel.he
-
 		wheel = game.add.sprite(game.width / 2, game.width / 2, "wheel");
 		// setting wheel registration point in its center
 		wheel.anchor.set(0.5);
@@ -94,14 +89,9 @@ playGame.prototype = {
 		// now we can spin the wheel again
 		canSpin = true;
 		// writing the prize you just won
-		//prizeText.text = slicePrizes[prize];
 		//alert(window.screen.availWidth);
 		//alert(window.screen.availHeight);
 		console.log(prize);
-		//var test1 = window.screen.availWidth;
-		//var test2 = window.screen.availHeight;
-		//document.getElementById('conteudo').innerHTML = test1+" por "+test2;
-		//document.getElementById('conteudo').innerHTML = 'Qual é o problema/processo a ser trabalhado?</br></br>[A] Testando...</br>[A] Testando...</br>[A] Testando...</br>[A] Testando...';
 
 		switch(prize) {
 			case 0:
@@ -121,9 +111,6 @@ playGame.prototype = {
 			document.getElementById('QA').innerHTML = action_array[1]['a'];
 			break;
 		}
-
-		//document.getElementById('conteudo').innerHTML = askingArray[1]['ask'];
-		//document.getElementById('QA').innerHTML = askingArray[1]['a'];
 		setTimeout(function(){$("#myModal").modal();}, 3000);
 
 
