efdac3c6e1e77a1726f65e7a376f9f44cad043dd
Filipe Natanael <filipenatanael@live.com>
2017-06-07 23:01:27 -0300

    Update Questions
    Update Questions

--- questions.js
+++ questions.js
@@ -1,21 +1,21 @@
 //--- Questions about PLAN ---
 var plan_array = {
   '1': {
-    'ask': 'PLAN PLAN PLAN Qual é o problema/processo a ser trabalhado?</br></br>[A] Testando...</br>[A] Testando...</br>[A] Testando...</br>[A] Testando...',
+    'ask': 'Um projeto bem elaborado é primordial para o ciclo PDCA, pois impede falhas futuras e gera um enorme ganho de tempo.',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
     'answer': 'Isso mesmo'
   },
   '2': {
-    'ask': 'asdasdasdasddasdasdad',
+    'ask': 'Paute o _______________ de acordo com a missão, visão e os valores da empresa, estabelecendo metas e objetivos e definindo o melhor caminho para atingi-los.',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
     'answer': 'Isso mesmo'
   },
   '3': {
-    'ask': 'asa123123123123123213dasdasdasddasdasdad',
+    'ask': 'Nessa etapa o objetivo é focar na parte estratégica do ciclo, ou seja, no levantamento e análise das informações',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
@@ -26,21 +26,21 @@ var plan_array = {
 //--- Questions about DO ---
 var do_array = {
   '1': {
-    'ask': 'DO DO DO?</br></br>[A] Testando...</br>[A] Testando...</br>[A] Testando...</br>[A] Testando...',
+    'ask': 'Após fazer um planejamento cuidadoso, coloque-o em prática e à risca, ou seja, procure não queimar etapas tampouco improvisar, para não comprometer todo o ciclo PDCA..',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
     'answer': 'Isso mesmo'
   },
   '2': {
-    'ask': 'asdasdasdasddasdasdad',
+    'ask': 'Fase que é subdividida em outras três etapas: treinamento de todos os funcionários e gestores envolvidos no projeto, seguido da realização propriamente dita e da “colheita” de dados para uma posterior avaliação',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
     'answer': 'Isso mesmo'
   },
   '3': {
-    'ask': 'asa123123123123123213dasdasdasddasdasdad',
+    'ask': 'Onde tudo aquilo previamente planejado é executado, gerando a necessidade de avaliar a qualidade do que está sendo feito.',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
@@ -51,21 +51,21 @@ var do_array = {
 //--- Questions about CHECK ---
 var check_array = {
   '1': {
-    'ask': 'CHECK CHECK CHECK?</br></br>[A] Testando...</br>[A] Testando...</br>[A] Testando...</br>[A] Testando...',
+    'ask': 'É o estágio do ciclo PDCA onde são identificadas possíveis brechas no projeto. As metas alcançadas e resultados obtidos são mensurados através dos dados coletados e do mapeamento de processos ao final da execução.',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
     'answer': 'Isso mesmo'
   },
   '2': {
-    'ask': 'asdasdasdasddasdasdad',
+    'ask': 'A __________ pode e deve ser feita de duas maneiras: paralelamente à execução, de modo a ter certeza que o trabalho está sendo bem feito, e ao final dela, para uma análise estatística mais abrangente que permita os ajustes e acertos necessários.',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
     'answer': 'Isso mesmo'
   },
   '3': {
-    'ask': 'asa123123123123123213dasdasdasddasdasdad',
+    'ask': 'Nessa etapa temos a verificação de tudo o que foi feito, comparando o que havia sido planejado com o resultado final e com consequentes problemas e falhas que possam ter ocorrido durante o processo.',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
@@ -76,21 +76,21 @@ var check_array = {
 //--- Questions about ACTION ---
 var action_array = {
   '1': {
-    'ask': 'ACTION ACTION ACTION?</br></br>[A] Testando...</br>[A] Testando...</br>[A] Testando...</br>[A] Testando...',
+    'ask': 'A “última” etapa, na qual são aplicadas ações corretivas de modo a estar sempre e continuamente aperfeiçoando o projeto.',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
     'answer': 'Isso mesmo'
   },
   '2': {
-    'ask': 'asdasdasdasddasdasdad',
+    'ask': 'É simultaneamente fim e começo, pois após uma minuciosa apuração do que tenha causado erros anteriores, todo o ciclo PDCA é refeito com novas diretrizes e parâmetros.',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
     'answer': 'Isso mesmo'
   },
   '3': {
-    'ask': 'asa123123123123123213dasdasdasddasdasdad',
+    'ask': 'É a realização das ações corretivas, que visam a correção das falhas encontradas durante o processo. Após a correção ser realizada, deve-se repetir o ciclo. ',
     'a': 'Testando...',
     'c': 'Testando...',
     'c': 'Testando...',
