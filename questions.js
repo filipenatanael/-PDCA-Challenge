f872fc9325375667fdba3ce18489e5f25674f370
Filipe Natanael <filipenatanael@live.com>
2017-06-11 14:32:44 -0300

    ..
    ..

--- questions.js
+++ questions.js
@@ -1,101 +1,23 @@
 //--- Questions about PLAN ---
 var questions_array = {
   '1': {
-    'question': '[P]Um projeto bem elaborado é primordial para o ciclo PDCA, pois impede falhas futuras e gera um enorme ganho de tempo.',
+    'question': 'Um projeto bem elaborado é primordial para o ciclo PDCA, pois impede falhas futuras e gera um enorme ganho de tempo.',
     'answer': 'P',
     'status': '0'
   },
   '2': {
-    'question': '[D]Paute o _______________ de acordo com a missão, visão e os valores da empresa, estabelecendo metas e objetivos e definindo o melhor caminho para atingi-los.',
+    'question': 'Após fazer um planejamento cuidadoso, coloque-o em prática e à risca, ou seja, procure não queimar etapas tampouco improvisar, para não comprometer todo o ciclo PDCA..',
     'answer': 'D',
     'status': '0'
   },
   '3': {
-    'question': '[C]Nessa etapa o objetivo é focar na parte estratégica do ciclo, ou seja, no levantamento e análise das informações',
+    'question': 'É o estágio do ciclo PDCA onde são identificadas possíveis brechas no projeto. As metas alcançadas e resultados obtidos são mensurados através dos dados coletados e do mapeamento de processos ao final da execução.',
     'answer': 'C',
     'status': '0'
   },
   '4': {
-    'question': '[A]Após fazer um planejamento cuidadoso, coloque-o em prática e à risca, ou seja, procure não queimar etapas tampouco improvisar, para não comprometer todo o ciclo PDCA..',
+    'question': 'A “última” etapa, na qual são aplicadas ações corretivas de modo a estar sempre e continuamente aperfeiçoando o projeto.',
     'answer': 'A',
     'status': '0'
-  },
-}
-
-
-
-//--- Questions about PLAN ---
-var plan_array = {
-  '1': {
-    'ask': 'Um projeto bem elaborado é primordial para o ciclo PDCA, pois impede falhas futuras e gera um enorme ganho de tempo.',
-    'a': 'Testando...',
-    'answer': 'P'
-  },
-  '2': {
-    'ask': 'Paute o _______________ de acordo com a missão, visão e os valores da empresa, estabelecendo metas e objetivos e definindo o melhor caminho para atingi-los.',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-  '3': {
-    'ask': 'Nessa etapa o objetivo é focar na parte estratégica do ciclo, ou seja, no levantamento e análise das informações',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-}
-
-//--- Questions about DO ---
-var do_array = {
-  '1': {
-    'ask': 'Após fazer um planejamento cuidadoso, coloque-o em prática e à risca, ou seja, procure não queimar etapas tampouco improvisar, para não comprometer todo o ciclo PDCA..',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-  '2': {
-    'ask': 'Fase que é subdividida em outras três etapas: treinamento de todos os funcionários e gestores envolvidos no projeto, seguido da realização propriamente dita e da “colheita” de dados para uma posterior avaliação',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-  '3': {
-    'ask': 'Onde tudo aquilo previamente planejado é executado, gerando a necessidade de avaliar a qualidade do que está sendo feito.',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-}
-
-//--- Questions about CHECK ---
-var check_array = {
-  '1': {
-    'ask': 'É o estágio do ciclo PDCA onde são identificadas possíveis brechas no projeto. As metas alcançadas e resultados obtidos são mensurados através dos dados coletados e do mapeamento de processos ao final da execução.',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-  '2': {
-    'ask': 'A __________ pode e deve ser feita de duas maneiras: paralelamente à execução, de modo a ter certeza que o trabalho está sendo bem feito, e ao final dela, para uma análise estatística mais abrangente que permita os ajustes e acertos necessários.',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-  '3': {
-    'ask': 'Nessa etapa temos a verificação de tudo o que foi feito, comparando o que havia sido planejado com o resultado final e com consequentes problemas e falhas que possam ter ocorrido durante o processo.',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-}
-
-//--- Questions about ACTION ---
-var action_array = {
-  '1': {
-    'ask': 'A “última” etapa, na qual são aplicadas ações corretivas de modo a estar sempre e continuamente aperfeiçoando o projeto.',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-  '2': {
-    'ask': 'É simultaneamente fim e começo, pois após uma minuciosa apuração do que tenha causado erros anteriores, todo o ciclo PDCA é refeito com novas diretrizes e parâmetros.',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
-  },
-  '3': {
-    'ask': 'É a realização das ações corretivas, que visam a correção das falhas encontradas durante o processo. Após a correção ser realizada, deve-se repetir o ciclo. ',
-    'a': 'Testando...',
-    'answer': 'Isso mesmo'
   },
 }
