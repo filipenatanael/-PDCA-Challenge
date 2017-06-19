4a5073c4c2702586445f22906384746898f13460
Filipe Natanael <filipenatanael@live.com>
2017-06-18 22:49:27 -0300

    Inserted quastions about do and check
    Inserted quastions about do and check

--- questions.js
+++ questions.js
@@ -91,63 +91,103 @@ var questions_array = {
     'status': '0'
   },
   '19': {
-    'question': ' ',
+    'question': 'Fase que é subdividida em outras três etapas: treinamento de todos os funcionários e gestores envolvidos no projeto, seguido da realização propriamente dita e da “colheita” de dados para uma posterior avaliação.',
     'answer': 'D',
     'status': '0'
   },
   '20': {
-    'question': ' ',
+    'question': 'Onde tudo aquilo previamente planejado é executado, gerando a necessidade de avaliar a qualidade do que está sendo feito.',
     'answer': 'D',
     'status': '0'
   },
   '21': {
-    'question': ' ',
+    'question': 'Significa colocar o planejamento em prática, isto é, executar o plano de ação previamente elaborado na etapa de planejamento do PDCA, de modo rigorosamente de acordo com o planejamento pré-estabelecido.',
     'answer': 'D',
     'status': '0'
   },
   '22': {
-    'question': ' ',
+    'question': 'No caso, com a condução do plano, as mudanças no processo e as observações sobre o mesmo, devem ser coletadas também os dados para a verificação do processo na próxima etapa do ciclo.',
     'answer': 'D',
     'status': '0'
   },
   '23': {
-    'question': ' ',
+    'question': 'Realizar o trabalho planejado, de acordo com o plano de ação (execução da garantia da qualidade, cumprimento dos padrões).',
     'answer': 'D',
     'status': '0'
   },
   '24': {
-    'question': ' ',
+    'question': 'A fase executiva da metodologia PDCA, na qual se deve executar todos – e cada um dos – processos; com isso, será implementado o plano de ação, a execução do processo e a confecção do produto em si.',
     'answer': 'D',
     'status': '0'
   },
   '25': {
-    'question': ' ',
+    'question': 'A coleta de dados deve ser feita também nessa etapa – embora essas informações sejam apenas utilizadas nas etapas posteriores.',
     'answer': 'D',
     'status': '0'
   },
   '26': {
-    'question': ' ',
+    'question': 'Aqui é preciso realizar todas as atividades que foram previstas e planejadas dentro do plano de ação.',
     'answer': 'D',
     'status': '0'
   },
   '27': {
-    'question': ' ',
+    'question': 'Execução da Ação: quando o plano é executado. Durante essa execução devem ser feitas verificações periódicas a fim de manter o controle e eliminar possíveis dúvidas que possam ocorrer ao longo da execução.',
     'answer': 'D',
     'status': '0'
   },
   '28': {
-    'question': ' ',
-    'answer': 'D',
+    'question': 'A __________ pode e deve ser feita de duas maneiras: paralelamente à execução, de modo a ter certeza que o trabalho está sendo bem feito, e ao final dela, para uma análise estatística mais abrangente que permita os ajustes e acertos necessários.',
+    'answer': 'C',
     'status': '0'
   },
   '29': {
-    'question': ' ',
-    'answer': 'D',
+    'question': 'Nessa etapa temos a verificação de tudo o que foi feito, comparando o que havia sido planejado com o resultado final e com consequentes problemas e falhas que possam ter ocorrido durante o processo.',
+    'answer': 'C',
     'status': '0'
   },
   '30': {
-    'question': ' ',
-    'answer': 'D',
+    'question': 'Nela deve-se avaliar o que foi feito durante a etapa de execução, fazendo comparações e identificando as diferenças entre o planejado e o que foi realizado.',
+    'answer': 'C',
+    'status': '0'
+  },
+  '31': {
+    'question': 'Devemos verificar o que foi aprendido durante a execução do plano, comparando os resultados com as predições que foram feitas na etapa de planejamento. (verificação dos padrões de qualidade).',
+    'answer': 'C',
+    'status': '0'
+  },
+  '32': {
+    'question': 'Medir ou avaliar o que foi feito, identificando a diferença entre o realizado e o que foi planejado no plano de ação (verificação do cumprimento dos padrões da qualidade).',
+    'answer': 'C',
+    'status': '0'
+  },
+  '33': {
+    'question': 'É chegado o momento de estudar e analisar profundamente os dados coletados na fase D. A partir disso, se pode fazer uma comparação bastante apurada sobre o que foi obtido e o que era esperado, na fase P.',
+    'answer': 'C',
+    'status': '0'
+  },
+  '34': {
+    'question': 'As diferenças precisarão ser levadas em conta – se positivas ou negativas, a fim de analisar todo o processo, de modo crítico.',
+    'answer': 'C',
+    'status': '0'
+  },
+  '35': {
+    'question': 'Após planejar e por em prática, o gestor precisa monitorar e avaliar constantemente os resultados obtidos com a execução das atividades.',
+    'answer': 'C',
+    'status': '0'
+  },
+  '36': {
+    'question': 'Avaliar processos e resultados, confrontando-os com o planejado, com objetivos, especificações e estado desejado, consolidando as informações, eventualmente confeccionando relatórios específicos.',
+    'answer': 'C',
+    'status': '0'
+  },
+  '37': {
+    'question': 'É a análise ou verifi-cação dos resultados alcançados e dados coletados.',
+    'answer': 'C',
+    'status': '0'
+  },
+  '38': {
+    'question': 'Pode ocorrer concomitantemente com a realização do plano quando se veri-ica se o trabalho está sendo feito da forma devida, ou após a execução quando são feitas análises estatísticas dos dados e verificação dos itens de controle. Nesta fase podem ser detectados erros ou falhas.',
+    'answer': 'C',
     'status': '0'
   },
 }
